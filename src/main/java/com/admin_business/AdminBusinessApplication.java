d81bbd0e267ef14d8e7388a989e1c4cd92da1d6d
kgh <gwanhyeon0525@gmail.com>
2025-01-05 22:39:10 +0900

    chore: setting project source

---
+++ src/main/java/com/admin_business/AdminBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.admin_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin_business/AdminBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
