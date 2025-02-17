f59d05154577ca2e397e08bae2e9184a24489f30
kgh <gwanhyeon0525@gmail.com>
2025-02-17 23:07:00 +0900

    fix: solved conflict

--- src/main/java/com/admin_business/domain/ArticleComment.java
+++ src/main/java/com/admin_business/domain/ArticleComment.java
@@ -1,17 +1,54 @@
-package com.admin_business.domain;
-
-import java.time.LocalDateTime;
-
-public class ArticleComment {
-
-  private Long id;
-  private Article article;
-  private String title;
-  private String content;
-  private String hashtag;
-  private LocalDateTime createdAt;
-  private String createdBy;
-  private LocalDateTime modifiedAt;
-  private String modifiedBy;
-
-}
+//package com.admin_business.domain;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//import java.util.Objects;
+//
+//@Getter
+//@ToString(callSuper = true)
+//@Table(indexes = {
+//        @Index(columnList = "content"),
+//        @Index(columnList = "createdAt"),
+//        @Index(columnList = "createdBy")
+//})
+//@Entity
+//public class ArticleComment extends AuditingFields {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+//    @Setter @ManyToOne(optional = false) @JoinColumn(name = "userId") private UserAccount userAccount; // 유저 정보 (ID)
+//
+//    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+//
+//
+//    protected ArticleComment() {}
+//
+//    private ArticleComment(Article article, UserAccount userAccount, String content) {
+//        this.article = article;
+//        this.userAccount = userAccount;
+//        this.content = content;
+//    }
+//
+//    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+//        return new ArticleComment(article, userAccount, content);
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof ArticleComment that)) return false;
+//        return id != null && id.equals(that.getId());
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
+//
+//}
